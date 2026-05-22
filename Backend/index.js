29944386ffa8e21ba0d4df2caedf2684e174c47e
theangelskies <theangelskies13@yahoo.com>
2026-05-22 23:08:54 +0200

    refactor index.js

--- Backend/index.js
+++ Backend/index.js
@@ -64,5 +64,5 @@ app.post("/messages/:id/dislike", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
